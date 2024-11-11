80bc7cd5ed866b38c24124d46dbb09eda83137dd
Carolina Saldana <gstechsolutions@gmail.com>
2024-11-11 16:19:54 -0600

    added unit test

---
+++ DevOpsApi.core.api.tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DevOpsApi.core.api.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var num1 = 10;
+            var num2 = 20;
+
+            var total = num1 + num2;
+
+            Assert.Equal(30, total);
+        }
+    }
+}
